9db25abf0011574e6b862c67270fb20992e5d586
Daniel <danserc117@gmail.com>
2019-11-25 16:51:15 -0600

    commit 2

--- app/src/main/java/com/example/transporte/MainActivity.java
+++ app/src/main/java/com/example/transporte/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.transporte;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.rutas);
+
+
 
     }
 
@@ -53,4 +60,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
         return false;
     }
+
+
 }
--- app/src/main/java/com/example/transporte/rutas.java
+++ app/src/main/java/com/example/transporte/rutas.java
@@ -1,19 +1,23 @@
 package com.example.transporte;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class rutas extends Fragment {
+public class rutas extends Fragment implements View.OnClickListener{
 
 
     public rutas() {
@@ -24,8 +28,35 @@ public class rutas extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rutas, container, false);
+        View v = inflater.inflate(R.layout.fragment_rutas, container, false);
+
+        CardView card_azul = v.findViewById(R.id.cardViewAzul);
+        card_azul.setOnClickListener(this);
+
+        CardView card_verde = v.findViewById(R.id.cardViewVerde);
+        card_verde.setOnClickListener(this);
+        return v;
+
+
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cardViewAzul:
+
+                Intent intent1 = new Intent(getActivity(), Rutas_activity_azul.class);
+                startActivity(intent1);
+
+                break;
+
+            case R.id.cardViewVerde:
+
+                Intent intent2 = new Intent(getActivity(), Rutas_activity_verde.class);
+                startActivity(intent2);
+
+                break;
+        }
+    }
 }
